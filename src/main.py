43a4a7cce26731199eeb22e38ac51369231dbffa
Guillaume Brocard <guillaume.brocard@epitech.eu>
2022-05-29 11:57:02 +0200

    Added logger

--- src/main.py
+++ src/main.py
@@ -1,10 +1,21 @@
 import asyncio
+import logging
+import sys
 from http import HTTPStatus
 
+from src.environment import WATCHER_FREQUENCY
 from tgtg import TgtgAPIError
 
 from src.notifications import TelegramNotificationService, MessageType
 from src.watcher import Watcher
+
+logger = logging.getLogger(__name__)
+
+
+def init_logger():
+    logging.basicConfig(stream=sys.stdout, level=logging.NOTSET,
+                        format='%(asctime)-2s | %(levelname)-5s - %(name)-10s -  %(message)s',
+                        datefmt="%Y-%m-%d %H:%M:%S")
 
 
 async def main():
@@ -14,6 +25,7 @@ async def main():
         await watcher.connect()
         await watcher.launch()
     except TgtgAPIError as ex:
+        logger.error(ex)
         status_code, msg = ex
         if status_code == HTTPStatus.TOO_MANY_REQUESTS:
             notifier.notify(message=msg, message_type=MessageType.ERROR)
@@ -21,8 +33,12 @@ async def main():
         else:
             notifier.notify(message=repr(ex), message_type=MessageType.ERROR)
     except Exception as ex:
+        logger.error(ex)
         notifier.notify(message=repr(ex), message_type=MessageType.ERROR)
+        await asyncio.sleep(WATCHER_FREQUENCY)
 
 
 if __name__ == "__main__":
+    init_logger()
     asyncio.run(main())
+
--- src/watcher/watcher.py
+++ src/watcher/watcher.py
@@ -1,5 +1,7 @@
 import asyncio
 import json
+import logging
+from logging import Logger
 from os.path import exists
 from typing import List
 
@@ -17,36 +19,44 @@ class Watcher:
     __is_running: bool = True
 
     __favorite_cache: List[Favorite] = None
+    __logger: Logger
 
     def __init__(self, notification_service: NotificationService):
+        self.__logger = logging.getLogger(__name__)
         self._notification_service = notification_service
 
     async def launch(self):
+        self.__logger.info("Started")
         while self.__is_running:
             # refresh credentials
             self.__credentials = Credentials(**self.__client.get_credentials())
 
             favorites = [Favorite(v['display_name'], v['items_available'], v['in_sales_window'])
                          for v in self.__client.get_items()]
-
+            self.__logger.info(f"Found {len(favorites)} favorites")
             # filter on new alerts only, or if Favorite is expired
             if self.__favorite_cache is not None:
                 favorites = [f for f in favorites if f not in self.__favorite_cache or f.is_expired]
 
-            # notify if favorite is available
-            [self._notification_service.notify(message=repr(favorite))
-             for favorite in favorites if favorite.is_available]
+            # filter on available
+            favorites = [favorite for favorite in favorites if favorite.is_available]
+            self.__logger.info(f"Found {len(favorites)} available favorites!")
+
+            [self._notification_service.notify(message=repr(favorite)) for favorite in favorites]
 
             self.__favorite_cache = favorites
 
+            self.__logger.debug("Sleeping...")
             await asyncio.sleep(WATCHER_FREQUENCY)
 
     async def connect(self):
         if exists("./credentials.json"):
+            self.__logger.debug("Reusing credentials stored in 'credentials.json' file")
             with open("./credentials.json", "r") as file:
                 tmp = json.loads(file.read())
                 self.__credentials = Credentials(**tmp)
         else:
+            self.__logger.debug("Connecting to TooGoodToGo API...")
             tmp = self.__client.get_credentials()
             self.__credentials = Credentials(**tmp)
             with open("./credentials.json") as file:
@@ -60,4 +70,5 @@ class Watcher:
                                    )
 
     def stop(self):
+        self.__logger.info("Stopping...")
         self.__is_running = False
